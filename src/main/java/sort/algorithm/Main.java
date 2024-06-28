6a7a1ee25713c9df34f433661e95dc8ba2ea6f0a
lucas <lucasluna04@gmail.com>
2024-06-27 21:26:53 -0300

    Resolvendo desafio de selectionSort

---
+++ src/main/java/sort/algorithm/Main.java
@@ -0,0 +1,33 @@
+package sort.algorithm;
+
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {64, 25, 12, 22, 11};
+
+        selectionSort(arr);
+
+        System.out.print("O array ordenado está aqui: ");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+
+    public static void selectionSort(int[] arr) {
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            int indiceMenor = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[indiceMenor]) {
+                    indiceMenor = j;
+                }
+            }
+            if (indiceMenor != i) {
+                int temp = arr[indiceMenor];
+                arr[indiceMenor] = arr[i];
+                arr[i] = temp;
+            }
+        }
+    }
+}
